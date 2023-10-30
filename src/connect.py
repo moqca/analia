f7030d9a3bfe6320000220100b2b66ff120322d1
and <mor.qca@gmail.com>
2023-10-30 09:18:34 -0700

    v2.0

--- src/connect.py
+++
@@ -1,9 +0,0 @@
-import snowflake.connector
-
-ctx = snowflake.connector.connect(user='WFM_USER',
-                                  password='3LpNAYnLpW79',
-                                  account='confie',
-                                  role='WFM_ROLE',
-                                  warehouse='DS_WH',
-                                  database='DATA_ANALYTICS',
-                                  schema='WFM_WORKSPACE')
